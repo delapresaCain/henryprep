75e9c4426d61253ac5103e8a3508acd2299206b7
cain <delapresa.cain@outlook.com>
2024-02-17 08:46:33 -0300

    modificated4e

--- M06 Bucles/ejercicios/06.js
+++ M06 Bucles/ejercicios/06.js
@@ -2,6 +2,9 @@ function tieneTresDigitos(num) {
   // Si el número recibido tiene tres dígitos, retorna true.
   // Caso contrario, retorna false.
   // Tu código:
+  // return num > 99 && num < 1000 ? true : false;
+if (num.toString().length === 3) return true;
+else return false;  
 }
 
 module.exports = tieneTresDigitos;
--- M06 Bucles/ejercicios/20.js
+++ M06 Bucles/ejercicios/20.js
@@ -3,6 +3,13 @@ function sumarHastaNConBreak(n) {
   // Devuelve la suma de todos los números desde 1 hasta n.
   // Si la suma supera a 100, detén el bucle usando break.
   // Tu código:
+  var acumuladore = 0;
+  for (let i = 1; i <= n; i++){
+    acumuladore = acumuladore + i
+  if (acumuladore > 100)
+    break;
+  }
+  return acumuladore;
 }
 
 module.exports = sumarHastaNConBreak;
